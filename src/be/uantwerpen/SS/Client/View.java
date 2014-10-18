d02107d34a68442b8325f308ade9c917ff3e252c
kevinheyvaert <kevin-heyvaert@hotmail.com>
2014-10-18 19:28:00 +0200

    Putted some Java example code in class to check if it is a Java project.

--- src/be/uantwerpen/SS/Client/View.java
+++ src/be/uantwerpen/SS/Client/View.java
@@ -1,5 +1,31 @@
 package be.uantwerpen.SS.Client;
 
 public class View {
+	
+public static void main(String args[]) {
+		
+		long StartTime , EndTime, TotalTime;
+		double pi = 4;
+		int [] anArray = new int[1000000];
+		
+		//start time
+		StartTime = System.nanoTime();
+		
+		System.out.println("Starting program SingleThread : ");
+		
+		System.out.println("Calculating pi...");
+		boolean plus = false;
+		for (double i = 3; i < 1000000000; i += 2) {
+			if (plus) {
+				pi += 4.0 / i;
+			} else {
+				pi -= 4.0 / i;
+			}
+			plus = !plus;
+		}
+		
+		System.out.println("Pi = " +pi);
+		
+}
 
 }
